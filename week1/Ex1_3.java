66cf28c1b078f96052696e39c319641af058ad6f
Silver Sirp <silver.sirp@voco.ee>
2022-09-08 23:55:13 +0300

    Week 1 - Ex1 to 3: printing

---
+++ week1/Ex1_3.java
@@ -0,0 +1,25 @@
+package week1;
+
+public class Ex1_3 {
+    public static void main(String[] args) {
+        Ex1();
+        Ex2();
+        Ex3();
+    }
+
+    public static void Ex1() {
+        System.out.println("John Doe");
+    }
+
+    public static void Ex2() {
+        System.out.println("Hello World!\n(and all the people of the World)");
+    }
+
+    public static void Ex3() {
+        System.out.println(" ");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println("*****");
+        System.out.println("  *  ");
+    }
+}
