c41b22a51e64a52c3bbd5f49c1c2827e7a0f251d
Silver Sirp <silver.sirp@voco.ee>
2022-09-09 00:44:07 +0300

    Week 1 - Ex19: if and logical operations

---
+++ week1/Ex19.java
@@ -0,0 +1,18 @@
+package week1;
+
+import java.util.Scanner;
+
+public class Ex19 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("How old are you? ");
+        int age = Integer.parseInt(reader.nextLine());
+
+        if (age < 0 || age > 120) {
+            System.out.println("Impossible!");
+        } else {
+            System.out.println("OK");
+        }
+    }
+}
