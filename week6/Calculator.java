462bc085283f378fd7e67e34a9741c98b44bfc6a
Silver Sirp <silver.sirp@voco.ee>
2022-09-15 11:50:33 +0300

    Week 6 - Ex3 Calculator

---
+++ week6/Calculator.java
@@ -0,0 +1,11 @@
+package week6;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.start();
+    }
+
+    private void start() {
+    }
+}
