5a3e813d647ed38c0b641a6aa50f11d79801ef2c
Silver Sirp <silver.sirp@voco.ee>
2022-09-15 09:12:50 +0300

    Week 5 - Ex93.1: Calculating the age based on the birthday

--- week5/Ex93.java
+++ week5/Ex93.java
@@ -1,4 +1,13 @@
 package week5;
 
 public class Ex93 {
+    public class Main {
+        public static void main(String[] args) {
+            Person pekka = new Person("Pekka", 15, 2, 1993);
+            Person steve = new Person("Thomas", 1, 3, 1955);
+
+            System.out.println( steve.getName() + " age " + steve.age() + " years");
+            System.out.println( pekka.getName() + " age " + pekka.age() + " years");
+        }
+    }
 }
---
+++ week5/Person.java
@@ -0,0 +1,29 @@
+package week5;
+
+import java.util.Calendar;
+
+public class Person {
+    private String name;
+    private MyDate birthday;
+
+    public Person(String name, int pp, int kk, int vv) {
+        this.name = name;
+        this.birthday = new MyDate(pp, kk, vv);
+    }
+
+    public int age() {
+        // calculate the age based on the birthday and the current day
+        // you get the current day as follows:
+        // Calendar.getInstance().get(Calendar.DATE);
+        // Calendar.getInstance().get(Calendar.MONTH) + 1; // January is 0 so we add one
+        // Calendar.getInstance().get(Calendar.YEAR);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String toString() {
+        return this.name +", born "+ this.birthday;
+    }
+}
