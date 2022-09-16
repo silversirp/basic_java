4aca891a4ec750f06c1897fccd966735ad63e3e1
Silver Sirp <silver.sirp@voco.ee>
2022-09-16 14:23:09 +0300

    Week 7 - Exercise 7.5: Adding and Translating Words

--- week7/TextUserInterface.java
+++ week7/TextUserInterface.java
@@ -8,19 +8,35 @@ public class TextUserInterface {
     public TextUserInterface(Scanner reader, Dictionary dictionary) {
         this.dictionary = dictionary;
         this.reader = reader;
+        this.dictionary.add("add", "adds a word pair to the dictionary");
+        this.dictionary.add("translate", "asks a word and prints its translation");
         this.dictionary.add("quit", "quit the text user interface");
     }
 
     public void start(){
         System.out.println("Statement: ");
-        System.out.println("quit - " + this.dictionary.translate("quit"));
+        for (String statement: this.dictionary.translationList()) {
+            System.out.println(statement);
+        }
         while(true){
             System.out.println("Statement: ");
             String input = reader.nextLine();
             if(this.dictionary.translate(input) == null){
                 System.out.println("Unknown statement");
             } else {
-                if(input.equals("quit")){
+                if(input.equals("add")){
+                    System.out.println("In Finnish:");
+                    String word = reader.nextLine();
+                    System.out.println("Translation:");
+                    String translation = reader.nextLine();
+                    this.dictionary.add(word, translation);
+                } if(input.equals("translate")){
+                    System.out.println("Give a word:");
+                    String word = reader.nextLine();
+                    System.out.println("Translation:");
+                    System.out.println(this.dictionary.translate(word));
+                }
+                else if(input.equals("quit")){
                     System.out.println("Cheers!");
                     break;
                 }
