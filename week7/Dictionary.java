42afba949b34eb2be5620475bd0784c42a128acb
Silver Sirp <silver.sirp@voco.ee>
2022-09-16 12:59:13 +0300

    Week 7 - Exercise 7.3: Listing All Words

--- week7/Dictionary.java
+++ week7/Dictionary.java
@@ -1,5 +1,6 @@
 package week7;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Dictionary {
@@ -22,5 +23,16 @@ public class Dictionary {
 
     public int amountOfWords(){
         return this.dictionary.size();
+    }
+
+    public ArrayList<String> translationList(){
+        ArrayList<String> transList = new ArrayList<String>();
+        for (String key:
+             this.dictionary.keySet()) {
+            String value = this.dictionary.get(key);
+            String pair = key + " = " + value;
+            transList.add(pair);
+        }
+        return transList;
     }
 }
--- week7/Ex7.java
+++ week7/Ex7.java
@@ -1,13 +1,17 @@
 package week7;
 
+import java.util.ArrayList;
+
 public class Ex7 {
     public static void main(String[] args) {
         Dictionary dictionary = new Dictionary();
         dictionary.add("apina", "monkey");
         dictionary.add("banaani", "banana");
-        System.out.println(dictionary.amountOfWords());
-
         dictionary.add("cembalo", "harpsichord");
-        System.out.println(dictionary.amountOfWords());
+
+        ArrayList<String> translations = dictionary.translationList();
+        for(String translation: translations) {
+            System.out.println(translation);
+        }
     }
 }
