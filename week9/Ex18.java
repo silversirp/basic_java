668d3f5ce394f36349f7c3b8f1673bac8fb28f23
Silver Sirp <silver.sirp@voco.ee>
2022-09-22 11:40:06 +0300

    Week 9 - Exercise 18.2: Comparable Item

--- week9/Ex18.java
+++ week9/Ex18.java
@@ -3,9 +3,18 @@ package week9;
 import week9.mooving.domain.Item;
 import week9.mooving.domain.Thing;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Ex18 {
     public static void main(String[] args) {
-        Thing item = new Item("toothbrash", 2);
-        System.out.println(item);
+        List<Item> items = new ArrayList<Item>();
+        items.add(new Item("passport", 2));
+        items.add(new Item("toothbrash", 1));
+        items.add(new Item("circular saw", 100));
+
+        Collections.sort(items);
+        System.out.println(items);
     }
 }
--- week9/mooving/domain/Item.java
+++ week9/mooving/domain/Item.java
@@ -1,6 +1,6 @@
 package week9.mooving.domain;
 
-public class Item implements Thing {
+public class Item implements Thing, Comparable<Item> {
     private String name;
     private int volume;
 
@@ -13,13 +13,26 @@ public class Item implements Thing {
         return name;
     }
 
+    public int getItemVolume() {
+        return volume;
+    }
+
     @Override
     public int getVolume() {
-        return this.volume;
+        return this.getItemVolume();
     }
 
     @Override
     public String toString() {
         return this.name + " (" + this.volume + " dm^3)";
+    }
+
+    @Override
+    public int compareTo(Item anotherItem) {
+        if(this.getVolume() == anotherItem.getVolume()){
+            return 0;
+        } else if(this.getVolume() > anotherItem.getVolume()){
+            return 1;
+        } else return -1;
     }
 }
