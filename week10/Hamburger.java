6c46d0069364617c397e952bca8841dd7ae0fbda
Silver Sirp <silver.sirp@voco.ee>
2022-09-30 10:37:12 +0300

    Week 10 - Exercise Burger - Basic burger with addons

--- week10/Hamburger.java
+++ week10/Hamburger.java
@@ -60,7 +60,27 @@ public class Hamburger {
     }
 
     public double itemizeHamburger(){
+        System.out.println("Basic hamburger on a " + this.breadRollType + " roll with " + this.meat + ", price is " + this.price);
 
+        if (addition1Name != null){
+            this.totalPrice += this.addition1Price;
+            System.out.println("Added " + this.addition1Name + " for an extra " + this.addition1Price);
+        }
+        if (addition2Name != null){
+            this.totalPrice += this.addition2Price;
+            System.out.println("Added " + this.addition2Name + " for an extra " + this.addition2Price);
+        }
+        if (addition3Name != null){
+            this.totalPrice += this.addition3Price;
+            System.out.println("Added " + this.addition3Name + " for an extra " + this.addition3Price);
+        }
+        if (addition4Name != null){
+            this.totalPrice += this.addition4Price;
+            System.out.println("Added " + this.addition4Name + " for an extra " + this.addition4Price);
+        }
+
+        //finally add basic price of burger
+        totalPrice += this.price;
         return totalPrice;
     }
 }
