25cdbfb44c63c9b74bada6546006c2bf318a4774
Silver Sirp <silver.sirp@voco.ee>
2022-09-30 11:02:45 +0300

    Week 10 - Exercise Burger - DeluxeBurger

--- week10/DeluxeBurger.java
+++ week10/DeluxeBurger.java
@@ -1,4 +1,7 @@
 package week10;
 
-public class DeluxeBurger {
+public class DeluxeBurger extends Hamburger {
+    public DeluxeBurger(String name, String meat, double price, String breadRollType) {
+        super(name, meat, price, breadRollType);
+    }
 }
